303f501822f1f6b7e0e9a5f4e7a8c83d0584db67
Evergreen <781804951@qq.com>
2017-07-03 20:42:16 +0800

    7-3
    commit

---
+++ 剑指offer/二叉搜索树的后序遍历序列.java
@@ -0,0 +1,13 @@
+package 算法;
+
+/**
+ * Created by ASDF on 2017/7/3.
+ */
+public class 二叉搜索树的后序遍历序列 {
+    public static boolean VerifySquenceOfBST(int [] sequence) {
+        return true;
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- 剑指offer/树的子结构.java
+++ 剑指offer/树的子结构.java
@@ -15,21 +15,32 @@ class TreeNode{
 public class 树的子结构 {
     public static boolean HasSubtree(TreeNode root1,TreeNode root2) {
         boolean result=false;
-        if(root1 !=null && root2 !=null){
-            result=judge(root1,root2);
+        if(root1!=null&&root2!=null){
+            if(root1.val==root2.val){
+                //递归判断子树
+                result=judge(root1,root2);
+            }
+            if(!result)
+                result=HasSubtree(root1.left,root2);
+            if(!result)
+                result=HasSubtree(root1.right,root2);
         }
-        if(result==false)
-            result=judge(root1.left,root2);
-        if(result==false)
-            result=judge(root1.right,root2);
         return result;
     }
     public static boolean judge(TreeNode root1, TreeNode root2){
-        if(root1==null&&root2!=null)
-            return false;
+        /*if(root2==null)
+            return true;
+        if(root1==null)
+            return false;*/
+
         if(root1==null&&root2==null)
             return true;
-        if(root1.val != root2.val)
+        if(root1==null&&root2!=null)
+            return false;
+        if(root1!=null&&root2==null)
+            return true;
+
+        if(root1.val!=root2.val)
             return false;
         return judge(root1.left,root2.left)&&judge(root1.right,root2.right);
     }
